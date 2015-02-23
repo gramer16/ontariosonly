f3e6a7a602e40ccadf6a573cdcc8f92f62fd82a7
graciem <gramer16@dnginternational.ca>
2015-02-23 08:58:38 -0600

    E

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,6 +1,5 @@
 class SubscriptionsController < ApplicationController
   before_action :set_subscription, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
   before_action :check_user, only: [:destroy]
   def index
     @subscriptions = Subscription.all
