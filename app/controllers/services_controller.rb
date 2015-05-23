467863d1092c6738bbfca2b3fd85f8786d6edb44
graciem <gramer16@dnginternational.ca>
2015-05-23 18:21:39 -0500

    H

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -72,7 +72,7 @@ class ServicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def service_params
-      params.require(:service).permit(:company_name, :service_type, :company_description, :address, :city, :zipcode, :contact_name, :company_website, :company_phone, :image)
+      params.require(:service).permit(:service_type, :company_name, :company_description, :address, :city, :zipcode, :contact_name, :company_website, :company_phone, :image)
     end
 
     def check_user
