cc59d6b5aec6fa985ac002e7b62e58dd3ed34ec0
graciem <gramer16@dnginternational.ca>
2015-05-24 01:59:59 -0500

    G

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150524010659) do
+ActiveRecord::Schema.define(version: 20150524013902) do
 
   create_table "accountants", force: true do |t|
     t.string   "company_name"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20150524010659) do
     t.text     "company_description"
     t.text     "address"
     t.string   "city"
-    t.integer  "zipcode"
+    t.string   "zipcode"
     t.string   "contact_name"
     t.string   "company_website"
     t.string   "company_phone"
