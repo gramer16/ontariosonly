cee4a1a564030b95d83c5a1e5aaffe6eed363539
graciem <gramer16@dnginternational.ca>
2015-05-25 20:31:11 -0500

    W

--- app/controllers/floorings_controller.rb
+++ app/controllers/floorings_controller.rb
@@ -1,5 +1,6 @@
 class FlooringsController < ApplicationController
   before_action :set_flooring, only: [:show, :edit, :update, :destroy]
+   before_action :check_user, only: [:destroy, :edit]
   def search
     if params[:search].present?
       @floorings = Flooring.search(params[:search])
@@ -23,7 +24,6 @@ class FlooringsController < ApplicationController
 
   def create
       @flooring = Flooring.new(flooring_params)
-       @flooring.user_id = current_user.id
     respond_to do |format|
       if  @flooring.save
         format.html { redirect_to  @flooring, notice: 'Service was successfully created.' }
@@ -63,4 +63,9 @@ end
     def flooring_params
       params.require(:flooring).permit(:company_name, :company_description, :address, :city, :zipcode, :contact_name, :company_website, :company_phone, :image)
     end
+    def check_user
+        unless current_user.admin?
+         redirect_to root_url, alert: "Sorry, Only Ontario's Only Admin can Delete a Subscription"
+    end
+  end
 end
