118ee46d2c5370e849875daff24a81bc91c56371
graciem <gramer16@dnginternational.ca>
2015-06-14 03:21:19 -0400

    S

--- app/controllers/molds_controller.rb
+++ app/controllers/molds_controller.rb
@@ -1,6 +1,6 @@
 class MoldsController < ApplicationController
   before_action :set_mold, only: [:show, :edit, :update, :destroy]
-
+  before_action :check_user, only: [:destroy, :edit]
   def search
     if params[:search].present?
       @molds = Mold.search(params[:search])
@@ -26,7 +26,6 @@ class MoldsController < ApplicationController
 
   def create
     @mold = Mold.new(mold_params)
-    @mold.user_id = current_user.id
     respond_to do |format|
       if @mold.save
         format.html { redirect_to @mold, notice: 'Service was successfully created.' }
@@ -66,4 +65,9 @@ end
     def mold_params
       params.require(:mold).permit(:company_name, :company_description, :address, :city, :zipcode, :contact_name, :company_website, :company_phone, :image)
     end
+    def check_user
+        unless current_user.admin?
+         redirect_to root_url, alert: "Sorry, Only Ontario's Only Admin can Delete a Subscription"
+    end
+  end
 end
