cc7b9f96c02738d7bef27479a5370beff2c60cea
graciem <gramer16@dnginternational.ca>
2015-06-13 20:01:26 -0400

    D

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,5 +1,6 @@
 class PaintingsController < ApplicationController
   before_action :set_painting, only: [:show, :edit, :update, :destroy]
+  before_action :check_user, only: [:destroy, :edit]
   def search
     if params[:search].present?
       @paintings = Painting.search(params[:search])
@@ -26,7 +27,6 @@ class PaintingsController < ApplicationController
   def create
 
      @painting = Painting.new(painting_params)
-    @painting.user_id = current_user.id
     respond_to do |format|
       if @painting.save
         format.html { redirect_to @painting, notice: 'Service was successfully created.' }
@@ -66,4 +66,9 @@ end
     def painting_params
       params.require(:painting).permit(:company_name, :company_description, :address, :city, :zipcode, :contact_name, :company_website, :company_phone, :image)
     end
+    def check_user
+        unless current_user.admin?
+         redirect_to root_url, alert: "Sorry, Only Ontario's Only Admin can Delete a Subscription"
+    end
+  end
 end
